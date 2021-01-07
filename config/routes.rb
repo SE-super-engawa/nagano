3d6ab8de74783316c7f760ce914d1237b42386b7
takako-K <takako.k828@gmail.com>
2021-01-07 11:57:05 +0000

    [Change]deviseに対応したController・viewのカスタマイズ

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,11 @@ Rails.application.routes.draw do
 
   devise_for :admins
 
-  devise_for :customers
+  devise_for :customers, controllers: {
+    sessions: 'public/sessions',
+    passwords: 'public/passwords',
+    registrations: 'public/registrations'
+  }
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
