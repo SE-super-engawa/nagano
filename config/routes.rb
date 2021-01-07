9792603839c4db3ea20be7bc74687f862ac2b177
takako-K <takako.k828@gmail.com>
2021-01-07 03:11:18 +0000

    [Add]topアクションview追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
 
+  namespace :public do
+    get 'homes/top'
+  end
+
+  namespace :public do
+    get 'homes/about'
+  end
+
   devise_for :admins
 
   devise_for :customers
