6385ed9932bd72d65a025f328820bb6603f42dd1
takako-K <takako.k828@gmail.com>
2021-01-08 05:40:35 +0000

    [Add]サインアップ時項目等追加

--- app/controllers/public/customers/registrations_controller.rb
+++ app/controllers/public/customers/registrations_controller.rb
@@ -2,6 +2,8 @@
 
 class Public::Customers::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
+  before_action :authenticate_customer!, only: [:edit, :update]
+  before_action :configure_permitted_parameters, if: :devise_controller?
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
@@ -39,6 +41,10 @@ class Public::Customers::RegistrationsController < Devise::RegistrationsControll
   # end
 
   # protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name, :first_name, :family_name_kana, :first_name_kana, :postal_code, :address, :phone_number])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
