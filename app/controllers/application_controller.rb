a5253fdb650eb2f7bc48adf777594599f99cf8b6
Ami-tnk <ec2-user@ip-172-31-23-70.ec2.internal>
2021-01-08 07:00:39 +0000

    [Add]routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  # ログイン後のリダイレクト先
+  def after_sign_in_path_for(resource)
+    customers_my_page_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,15 @@ Rails.application.routes.draw do
 
   scope module: :public do
     resources :shipping_addresses, except:[:new, :show]
-    resource :customers, only: [:edit, :update]
+
     get 'customers/my_page', to: 'customers#show'
     get 'customers/check', to: 'customers#check'
     patch 'customers/withdraw', to: 'customers#withdraw'
+    resource :customers, only: [:edit, :update]
+
+    get 'orders/thanks', to: 'orders#thanks'          #resources :orderより前に記載必須
+    post 'orders/confirm', to: 'orders#confirm'       #resources :orderより前に記載必須
+    resources :orders, only: [:new, :index, :show, :create]
   end
 
   devise_for :admins
