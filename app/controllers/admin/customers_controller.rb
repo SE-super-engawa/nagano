859f17b45366072dce25916feadfd7d3381573f9
yuya-nagasaka <nagasaka.yuya0928@gmail.com>
2021-01-08 07:46:25 +0000

    update機能実装

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -13,5 +13,13 @@ class Admin::CustomersController < ApplicationController
   end
 
   def update
+    customer = Customer.find(params[:id])
+    customer.update(customer_params)
+    redirect_to admin_customer_path(customer)
+  end
+  
+  private
+  def customer_params
+    params.require(:customer).permit(:family_name, :first_name, :family_name_kana, :first_name_kana, :postal_code, :address , :phone_number, :email, :is_deleted)
   end
 end
