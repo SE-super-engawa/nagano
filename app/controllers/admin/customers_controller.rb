676114747ffca9499c2d5d244b4a2c5b64a55e8b
yuya-nagasaka <nagasaka.yuya0928@gmail.com>
2021-01-08 07:32:38 +0000

    editページ作成

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -8,8 +8,8 @@ class Admin::CustomersController < ApplicationController
     @customer = Customer.find(params[:id])
   end
 
-
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
