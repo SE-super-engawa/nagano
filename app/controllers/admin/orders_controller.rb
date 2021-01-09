042c741219da9ce5d7c86190a802bc6453641ac5
yuya-nagasaka <nagasaka.yuya0928@gmail.com>
2021-01-09 03:45:00 +0000

    [fix]indexページ 購入個数一覧

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -4,11 +4,10 @@ class Admin::OrdersController < ApplicationController
     path = Rails.application.routes.recognize_path(request.referer)
     if path[:controller] == "admins/customers" && path[:action] == "show"
       @orders = Order.where(customer_id: path[:id])
-    else 
-　    @orders = Order.page(params[:page]).reverse_order
-　  end
+    else
+      @orders = Order.page(params[:page]).reverse_order
+    end
   end
-
   def show
   end
 
