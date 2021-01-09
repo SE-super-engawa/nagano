c3fac90f00bd53753375a7ad0cad34bb0441a2f1
yuya-nagasaka <nagasaka.yuya0928@gmail.com>
2021-01-09 09:03:47 +0000

    [Add]showページ

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -8,7 +8,13 @@ class Admin::OrdersController < ApplicationController
       @orders = Order.page(params[:page]).reverse_order
     end
   end
+  
   def show
+    @order = Order(params[:id])
+    @total_price = 0
+    @order.order_products each do |order_product|
+      @total_price += order_product.price*order_product.quantity
+    end
   end
 
   def update
