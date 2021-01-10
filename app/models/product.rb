6d57b88e7e243ef5381de241f886e3c2c7c2da91
takako-K <takako.k828@gmail.com>
2021-01-10 10:46:57 +0000

    [Pull]admin_productsのpull

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ApplicationRecord
 
   has_many :cart_items, dependent: :destroy
   has_many :order_products, dependent: :destroy
-  belongs_to :genres, dependent: :destroy
+  belongs_to :genre, dependent: :destroy
 
   attachment :image_id
 
